bd01d0e7714b16b79845077870c09674708ab9e4
kklai <kkrebeccalai@gmail.com>
2014-01-05 17:27:51 -0600

    generating calendar

--- script.js
+++ script.js
@@ -1,5 +1,89 @@
+var cal_days_labels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+cal_months_labels = ['January', 'February', 'March', 'April',
+                     'May', 'June', 'July', 'August', 'September',
+                     'October', 'November', 'December'],
+cal_days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+cal_current_date = new Date();
+
+function Calendar(month, year) {
+  this.month = (isNaN(month) || month == null) ? cal_current_date.getMonth() : month;
+  this.year  = (isNaN(year) || year == null) ? cal_current_date.getFullYear() : year;
+  this.html = '';
+}
+
+Calendar.prototype.generateHTML = function(){
+
+  // get first day of month
+  var firstDay = new Date(this.year, this.month, 1);
+  var startingDay = firstDay.getDay();
+  
+  // find number of days in month
+  var monthLength = cal_days_in_month[this.month];
+  
+  // compensate for leap year
+  if (this.month == 1) { // February only!
+    if((this.year % 4 == 0 && this.year % 100 != 0) || this.year % 400 == 0){
+      monthLength = 29;
+    }
+  }
+  
+  // do the header
+  var monthName = cal_months_labels[this.month]
+  var html = '<table class="calendar-table">';
+  html += '<tr><th colspan="7">';
+  html +=  monthName + "&nbsp;" + this.year;
+  html += '</th></tr>';
+  html += '<tr class="calendar-header">';
+  for(var i = 0; i <= 6; i++ ){
+    html += '<td class="calendar-header-day">';
+    html += cal_days_labels[i];
+    html += '</td>';
+  }
+  html += '</tr><tr>';
+
+  // fill in the days
+  var day = 1;
+  // this loop is for is weeks (rows)
+  for (var i = 0; i < 9; i++) {
+    // this loop is for weekdays (cells)
+    for (var j = 0; j <= 6; j++) { 
+      html += '<td class="calendar-day"><p>';
+      if (day <= monthLength && (i > 0 || j >= startingDay)) {
+        html += day;
+        day++;
+      }
+      html += '</p></td>';
+    }
+    // stop making rows if we've run out of days
+    if (day > monthLength) {
+      break;
+    } else {
+      html += '</tr><tr>';
+    }
+  }
+  html += '</tr></table>';
+
+  this.html = html;
+}
+
+Calendar.prototype.getHTML = function() {
+  return this.html;
+}
+
+function drawCalendar() {
+	var cal = new Calendar();
+  cal.generateHTML();
+  $('.calendar').html(cal.getHTML());
+}
+
+function getCalendar() {
+	$.getJSON("http://www.google.com/calendar/feeds/s6bvn4u675jmcefa3q4337qajc@group.calendar.google.com/public/full?alt=json-in-script&max-results=25&singleevents=false&sortorder=ascending&orderby=starttime&callback=?",    
+  function(json){
+    console.log(json);
+	})
+}
+
 $(document).ready(function(){
-	for (var i = 1; i < 8; i ++ ) {
-    $('.calendar').append('<div class="cal-box"><p>' + i +'</p></div>')
-	}
-})
+	drawCalendar();
+	getCalendar();
+});
